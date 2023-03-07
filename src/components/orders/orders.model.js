466aad5c62c85449dd33d23f40d60588b386b265
Omar Saeed <omar.ahmed.saeed.1999@gmail.com>
2023-03-07 21:34:07 +0200

    fix: update mongoose orders model

--- src/components/orders/orders.model.js
+++ src/components/orders/orders.model.js
@@ -1,26 +1,27 @@
 import mongoose from "mongoose";
 
 const orderSchema = new mongoose.Schema({
-  orderId: { type: String, required: true, unique: true },
-  customerId: { type: String, required: true },
-  orderedBooks: { type: [Object], required: true },
-  orderDate: { type: Date, required: true },
+  orderId: { type: Number, required: true, unique: true },
+  customerId: { type: mongoose.SchemaTypes.ObjectId, ref: "User", required: true },
+  orderedBooks: [{ type: mongoose.SchemaTypes.ObjectId, ref: "Book", required: true }],
+  orderDate: { type: Date, default: Date.now },
   shippingDate: { type: Date },
   shippingAddress: {
-    street: String,
-    city: String,
-    state: String,
-    postalCode: String,
+    street: {
+      type: String,
+      required: true,
+    },
+    city: {
+      type: String,
+      required: true,
+    },
+    postalCode: {
+      type: String,
+    },
   },
   orderStatus: { type: String, enum: ["Pending", "Delivered", "Cancelled"], default: "Pending" },
-  orderTotal: {
-    type: Number,
-    required: true,
-  },
-  notes: {
-    type: String,
-    default: "",
-  },
+  orderTotal: { type: Number, required: true },
+  notes: { type: String, default: "" },
 });
 
 const Order = mongoose.model("Order", orderSchema);
