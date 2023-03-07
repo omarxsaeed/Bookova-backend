077f90e3f43c436b6addc91e0e84c0f9f88c44c2
Omar Saeed <omar.ahmed.saeed.1999@gmail.com>
2023-03-07 02:09:05 +0200

    add: single book route

--- src/components/books/books.route.js
+++ src/components/books/books.route.js
@@ -5,8 +5,10 @@ import * as booksValidation from "./books.validation.js";
 
 const router = Router();
 
-router.get("/", booksController.getAllBooks);
+router.get("/", booksController.getBook);
+router.get("/all", booksController.getAllBooks);
 router.post("/", validateSchema(booksValidation.addBook, "body"), booksController.addBook);
 router.put("/", validateSchema(booksValidation.updateBookInfo, "body"), booksController.updateBookInfo);
 router.delete("/", validateSchema(booksValidation.deleteBook, "body"), booksController.deleteBook);
+
 export default router;
