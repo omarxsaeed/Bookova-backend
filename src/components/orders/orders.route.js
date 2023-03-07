137286154abf4f4beaf9fbb1ecf74af0c4e3c3f9
Omar Saeed <omar.ahmed.saeed.1999@gmail.com>
2023-03-07 21:01:32 +0200

    feat: protect orders routes

--- src/components/orders/orders.route.js
+++ src/components/orders/orders.route.js
@@ -6,8 +6,8 @@ import * as ordersValidation from "./orders.validation.js";
 
 const router = Router();
 
-router.get("/", ordersController.getOrder);
-router.get("/all", ordersController.getAllOrders);
-router.post("/", validateSchema(ordersValidation.makeOrder, "body"), ordersController.makeOrder);
+router.get("/", isAuth, ordersController.getOrder);
+router.get("/all", isAuth, ordersController.getAllOrders);
+router.post("/", isAuth, validateSchema(ordersValidation.makeOrder, "body"), ordersController.makeOrder);
 
 export default router;
