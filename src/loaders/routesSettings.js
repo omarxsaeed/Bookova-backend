4e503862a83f37042c4ac9f4c6bfbbf8e24e4cb9
Omar Saeed <omar.ahmed.saeed.1999@gmail.com>
2023-03-07 15:38:47 +0200

    fix: add Content-Type Headers to solve CORS problems

--- src/loaders/routesSettings.js
+++ src/loaders/routesSettings.js
@@ -5,7 +5,7 @@ const routesSettings = (app) => {
   app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Methods", "OPTIONS, GET, POST, PUT, PATCH, DELETE");
-    res.setHeader("Access-Control-Allow-Headers", "Authorization");
+    res.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type");
     next();
   });
 };
